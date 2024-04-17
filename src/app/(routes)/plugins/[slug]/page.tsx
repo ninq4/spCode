da20ebc0c62bb4b0119fef1c61b33a5e7164598a
ninq4 <nik.dolgov.05@mail.ru>
2024-04-17 14:40:28 +0300

    Развернул базовую структуру приложения

---
+++ src/app/(routes)/plugins/[slug]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function singlePlugin({params} : {params: string}) {
+  return (
+    <div>page</div>
+  )
+}
---
+++ src/app/(routes)/plugins/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function plugins() {
+  return (
+    <div>page</div>
+  )
+}
